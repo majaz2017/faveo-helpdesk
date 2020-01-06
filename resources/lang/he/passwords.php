14685d72f0fecba85ab47616c6a7fc3a5202d71d
yousefkadah <it@alexor.co.il>
2020-01-06 15:42:42 +0200

    update for he lang

--- resources/lang/he/passwords.php
+++ resources/lang/he/passwords.php
@@ -13,9 +13,9 @@ return [
       |
      */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'user'     => "We can't find a user with that e-mail address.",
-    'token'    => 'This password reset token is invalid.',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'reset'    => 'Your password has been reset!',
+    'password' => 'על סיסמאות לכלול לפחות שש תווים ותואמים לאישור.',
+    'user'     => "איננו יכולים למצוא משתמש עם כתובת דואר אלקטרוני זו.",
+    'token'    => 'אסימון איפוס סיסמה זה אינו חוקי.',
+    'sent'     => 'שלחנו בדואל את קישור איפוס הסיסמה שלך!',
+    'reset'    => 'הסיסמה שלך אופסה!',
 ];
