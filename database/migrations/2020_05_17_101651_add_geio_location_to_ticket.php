e07ad5f47eb86f7fc2b21559e53161537991cc7e
Yahya <tech1@lasega.com>
2020-05-17 13:39:09 +0300

    add geio location to ticket

---
+++ database/migrations/2020_05_17_101651_add_geio_location_to_ticket.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGeioLocationToTicket extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Tickets', function (Blueprint $table) {
+            $table->float('latitude', 10, 6);
+            $table->float('longitude', 10, 6);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Tickets', function (Blueprint $table) {
+            $table->dropColumn('latitude');
+            $table->dropColumn('latitude');
+        });
+    }
+}
